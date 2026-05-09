3b8a23fb537d331fc41b95abaaf3fc8333146dd7
Oleg Bortovsky <oleg.bortovsky@gmail.com>
2026-05-09 10:41:38 +0300

    Complete task 1

--- task_1.py
+++ task_1.py
@@ -1,4 +1,18 @@
-import datetime
+from datetime import datetime
 
-def get_days_from_today(dete):
-    pass
+def get_days_from_today(date):
+    try:
+        target_date = datetime.strptime(date, "%Y-%m-%d").date()
+    except ValueError:
+        print("Date formate should be 'YYYY-mm-dd'")
+        return None
+
+    now_date = datetime.today().date()
+
+    delta = now_date - target_date
+
+    return delta.days
+
+
+result = get_days_from_today("2026-05-06")
+print(f"result: {result} days")
